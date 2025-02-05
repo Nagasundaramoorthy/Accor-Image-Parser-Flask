7a48acf89fc7428ec6ff03ead247fd17dd6c7291
Nagasundaramoorthy2023 <132643280+Nagasundaramoorthy2023@users.noreply.github.com>
2025-02-05 11:00:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,7 @@ genai.configure(api_key=gemini_key)
 gemini_model = genai.GenerativeModel('gemini-1.5-flash')
 
 # Configure Hugging Face embeddings and LLM
-embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2", 
-                                   model_kwargs={"device": "cpu", "torch_dtype": "auto"})
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
 llm = HuggingFaceHub(repo_id="google/flan-t5-large", model_kwargs={"temperature": 0.9, "max_length": 500})
 
